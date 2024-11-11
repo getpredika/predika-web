40c5a88d0e8e6aa3b6be36ba96b4ea7ff70d5b8d
itzmars <marsdoulens@proton.me>
2024-11-11 10:36:52 -0800

    add dictionary feature

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,3 +1,6 @@
+// src/components/ui/input.jsx
+import React from 'react';
+
 export const Input = ({ className, ...props }) => (
   <input
     className={`w-full px-4 py-2 border border-[#ddd] rounded-md 
